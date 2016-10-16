f6eb5bac577348ed419608cbf345b9536dfb12fc
Prithvi Prabahar <prithvi923@gmail.com>
2016-10-15 17:42:40 -0700

    Add Cocoapods

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
 }
 
